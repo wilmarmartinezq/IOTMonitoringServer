418596c6260038e7843222e505c6a381e324f587
wamartinezq <w.martinezq@uniandes.edu.co>
2022-09-22 13:51:07 -0500

    Ips

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "44.203.178.117"]
+ALLOWED_HOSTS = ["localhost", "100.27.28.197"]
 
 
 # Application definition
@@ -95,7 +95,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "34.238.250.87",  # Dirección IP de la base de datos
+        "HOST": "44.201.232.34",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -155,7 +155,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "44.203.141.8"
+MQTT_HOST = "3.82.155.29"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
